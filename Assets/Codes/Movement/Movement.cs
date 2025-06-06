e599440d051160f534a6f75cc5bb80d483d8ff07
LogicLover50 <hpaentser.50@gmail.com>
2025-06-07 05:59:12 +0700

    Colliders Update 07/06/25

--- Assets/Codes/Movement/Movement.cs
+++ Assets/Codes/Movement/Movement.cs
@@ -5,10 +5,10 @@ using UnityEngine.InputSystem;
 
 public class Movement : MonoBehaviour
 {
-    [SerializeField] 
-    private float XSpeed = 8f;
-    [SerializeField] 
-    private float YSpeed = 4f;
+    [SerializeField]
+    public float XSpeed = 8f;
+    [SerializeField]
+    public float YSpeed = 4f;
     private Vector2 moveSpeed;
     private Vector2 moveInput;
     private Rigidbody2D RbD;
@@ -16,13 +16,39 @@ public class Movement : MonoBehaviour
     void Start()
     {
         // Insert x & y values into vector
-        moveSpeed = new Vector2(XSpeed,YSpeed);
-        
+        moveSpeed = new Vector2(XSpeed, YSpeed);
+
         // Get Rigidbody values
         RbD = GetComponent<Rigidbody2D>();
 
         // Movement Tutorial
         // ?
+    }
+
+    private void OnTriggerEnter2D(Collider2D Collide)
+    {
+        if (Collide.CompareTag("StairsDown"))
+        {
+            YSpeed = 8;
+        }
+
+        if (Collide.CompareTag("StairsUp"))
+        {
+            YSpeed = 1;
+        }
+    }
+    
+    private void OnTriggerExit2D(Collider2D Collide)
+    {
+        if (Collide.CompareTag("StairsDown"))
+        {
+            YSpeed = 4;
+        }
+
+        if (Collide.CompareTag("StairsUp"))
+        {
+            YSpeed = 4;
+        }
     }
 
     void Update()
